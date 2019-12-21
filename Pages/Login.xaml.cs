4003ed189292db3a10295e4bc2f141bc225ab3b3
manhtrangtn <manhtrangtn@gmail.com>
2019-12-21 18:29:53 +0700

    Update

--- Pages/Login.xaml.cs
+++ Pages/Login.xaml.cs
@@ -12,7 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using T1809E_UWP_DAPI_D101_TrangDM2.Utils;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace T1809E_UWP_DAPI_D101_TrangDM2.Pages
@@ -20,12 +20,32 @@ namespace T1809E_UWP_DAPI_D101_TrangDM2.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+   
     public sealed partial class Login : Page
     {
+        private Validator validator = new Validator();
         public Login()
         {
             this.InitializeComponent();
         }
 
+        private void LoginHanlde(object sender, RoutedEventArgs e)
+        {
+            if (validator.IsEmail(email.Text)){
+                emailAlert.Text = "";
+                if (validator.IsNotNullAndNotEmpty(password.Password)){
+                    passwordAlert.Text = "";
+                    //Next
+                }
+                else
+                {
+                    passwordAlert.Text = "Please enter password";
+                }
+            }
+            else
+            {
+                emailAlert.Text = "Inlavid email, please enter valid email format.";
+            }
+        }
     }
 }
